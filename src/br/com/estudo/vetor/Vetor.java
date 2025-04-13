7e035587188d955cf848107193a7a7ede7d224f5
Beatriz Gomes <beanascigom@gmail.com>
2025-04-13 14:52:28 -0300

    Tornando o metodo adiciona() mais eficiente

--- src/br/com/estudo/vetor/Vetor.java
+++ src/br/com/estudo/vetor/Vetor.java
@@ -2,17 +2,19 @@ package br.com.estudo.vetor;
 
 public class Vetor {
 	private String[] elementos;
+	private int tamanho;
 	
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
 	}
 	
-	public void adiciona(String elemento) {
-		for (int i = 0; i <= this.elementos.length; i++) {
-			if (this.elementos[i] == null) {
-				elementos[i] = elemento;
-				break;
-			}
+	public void adiciona(String elemento) throws Exception {
+		if (this.tamanho < this.elementos.length) {
+			this.elementos[tamanho] = elemento;
+			this.tamanho++;
+		} else {
+			throw new Exception("Todas as posicoes do vetor estao ocupadas");
 		}
 	}
 }
--- src/br/com/estudo/vetor/teste/Aula03.java
+++ src/br/com/estudo/vetor/teste/Aula03.java
@@ -6,8 +6,13 @@ public class Aula03 {
 	public static void main(String[] args) {
 		Vetor vetor = new Vetor(3);
 		
-		vetor.adiciona("elemento 1");
-		vetor.adiciona("");
-		vetor.adiciona("elemento 3");
+		try {
+			vetor.adiciona("elemento 1");
+			vetor.adiciona("");
+			vetor.adiciona("elemento 3");			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
